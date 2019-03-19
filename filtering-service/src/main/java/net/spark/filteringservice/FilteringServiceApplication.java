4bae21bc057db2c7fbad7e757e3b290b7d0ab8fd
Daniel Thales Naves <tqi_dthales@uolinc.com>
2019-03-19 13:16:59 -0300

    Initial Spring Boot project configuration.

---
+++ filtering-service/src/main/java/net/spark/filteringservice/FilteringServiceApplication.java
@@ -0,0 +1,13 @@
+package net.spark.filteringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilteringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilteringServiceApplication.class, args);
+	}
+
+}
