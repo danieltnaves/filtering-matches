3b95d878e14fbb2506726049754e0874b703d537
Daniel <daniel.naves@outlook.com>
2019-03-24 21:26:14 -0300

    Add user info on top bar

--- matches-ui/src/components/Header.js
+++ matches-ui/src/components/Header.js
@@ -2,21 +2,40 @@ import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    }
+  };
 
 class Header extends Component {
     render () {
+        const { classes } = this.props
         return (
-            <React.Fragment>
-                <CssBaseline />
-                <AppBar position="static">
-                    <Toolbar>
-                    <Typography variant="h6" color="inherit" noWrap>Filtering Matches</Typography>
-                    </Toolbar>
-                </AppBar>
-            </React.Fragment>
+            <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6" color="inherit" className={classes.grow}>
+                    Filtering Matches
+                </Typography>
+                <Chip
+                    avatar={<Avatar>MS</Avatar>}
+                    label="Maria - London"
+                />
+              </Toolbar>
+            </AppBar>
+          </div>
         );
     }
 }
 
-export default Header;
+export default withStyles(styles)(Header);
